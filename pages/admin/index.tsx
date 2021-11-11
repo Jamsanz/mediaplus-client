5deb55abdf1d5eb39214579dc32ed33759269cc9
Muhammad Sani <jamiludeensani@gmail.com>
2021-11-11 21:06:37 +0100

    'Latest'

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,22 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminLayout from './layout'
 import Paper from '@mui/material/Paper'
 import Contacts from '@components/contact-report'
 import router from 'next/router'
+import { Spinner } from 'react-bootstrap'
 
 const Home: React.FC = (): JSX.Element => {
+  const [loaded, setLoaded] = useState<boolean>(false);
   useEffect(()=>{
     if (!(window.localStorage.getItem('MediaUser'))) {
       router.push('/admin/signIn');
+    }else{
+      setLoaded(true);
     }
   }, []);
 
     return (
+        loaded ? 
         <AdminLayout>
           <Paper>
             <Contacts />
           </Paper>
-        </AdminLayout>
+        </AdminLayout> 
+        : 
+        <div className="place-center">
+          <Spinner animation="border" variant="primary"/>
+        </div>
     )
 }
 

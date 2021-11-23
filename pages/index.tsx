abcb922a9e32a73d8e1d59578e2aec9dd8ab76b1
Muhammad Sani <jamiludeensani@gmail.com>
2021-11-23 20:51:40 +0100

    'Latest'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React,{useEffect} from "react";
 
 import { Main, Cards } from "@components";
 import Layout from "@components/layout";
 import ContactForm from "@components/contact-form";
 import Section from "@components/section";
 import Portfolio from "@components/section/portfolio";
+import { http } from "../utils/utils";
 
 const Home: React.FC = (): JSX.Element => {
+  useEffect(() => {
+    http.get('/');
+  }, [])
   return (
     <div className="d-flex flex-column min-vh-100">
       <Layout>

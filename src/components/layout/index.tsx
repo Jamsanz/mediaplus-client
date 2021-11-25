95c6918e80c530c84b969fe199970c334f6eebf3
Muhammad Sani <jamiludeensani@gmail.com>
2021-11-25 15:11:01 +0100

    'Latest'

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -22,12 +22,17 @@ const Layout = ({ children }: any): JSX.Element => {
         <meta property="description" content="Media Consultancy and Campaign outsource" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <meta name="description" content="Media Consultancy and Campaign outsource" />
+
+        {/* Open Graph / Facebook */}
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="@Media+ Consultancy | Media consultants" />
         <meta property="og:description" content="Media Consultancy and Campaign outsource" />
         <meta property="og:url" content="https://www.atmediaplus-consultancy.org" />
         <meta property="og:site_name" content="@Media+ Consultancy" />
+        <meta property="og:image" content="https://www.atmediaplus-consultancy.org/images/mediapluslogo.png" />
+
+        {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="@Media+ Consultancy | Media consultants" />
         <meta name="twitter:description" content="Media Consultancy and Campaign outsource" />

f8647be877278d5dbc1becddb8217628264e4023
Muhammad Sani <jamiludeensani@gmail.com>
2021-11-25 13:46:16 +0100

    'Latest'

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,7 +5,7 @@ import React, { useEffect } from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-const Layout = ({children}:any): JSX.Element => {
+const Layout = ({ children }: any): JSX.Element => {
   useEffect(() => {
     AOS.init({
       duration: 1000,
@@ -15,34 +15,34 @@ const Layout = ({children}:any): JSX.Element => {
     });
     AOS.refresh();
   }, []);
-    return (
-        <>
-        <Head>
-          <meta property="title" content="@Media+ Consultancy | Media consultants" />
-          <meta property="description" content="Media Consultancy and Campaign outsource" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-          <meta name="description" content="Neoistone is a reliable, fast and robust platform for all the needs of Hosting and Domain name development tools is so easy to use that litespeed it makes fast site" />
-          <meta property="og:locale" content="en_US" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="@Media+ Consultancy | Media consultants" />
-          <meta property="og:description" content="Media Consultancy and Campaign outsource" />
-          <meta property="og:url" content="https://www.atmediaplus-consultancy.org" />
-          <meta property="og:site_name" content="@Media+ Consultancy" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="@Media+ Consultancy | Media consultants" />
-          <meta name="twitter:description" content="Media Consultancy and Campaign outsource" />
-          <meta name="twitter:image" content="https://www.atmediaplus-consultancy.org/images/mediapluslogo.png" />
-          <title>@Media+ Consultancy | Media consultants</title>
-          <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet"/>
-          <link rel="icon" href="/images/mediapluslogo.png" />
-        </Head>
+  return (
+    <>
+      <Head>
+        <meta property="title" content="@Media+ Consultancy | Media consultants" />
+        <meta property="description" content="Media Consultancy and Campaign outsource" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="description" content="Neoistone is a reliable, fast and robust platform for all the needs of Hosting and Domain name development tools is so easy to use that litespeed it makes fast site" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="@Media+ Consultancy | Media consultants" />
+        <meta property="og:description" content="Media Consultancy and Campaign outsource" />
+        <meta property="og:url" content="https://www.atmediaplus-consultancy.org" />
+        <meta property="og:site_name" content="@Media+ Consultancy" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="@Media+ Consultancy | Media consultants" />
+        <meta name="twitter:description" content="Media Consultancy and Campaign outsource" />
+        <meta name="twitter:image" content="https://www.atmediaplus-consultancy.org/images/mediapluslogo.png" />
+        <title>@Media+ Consultancy | Media consultants</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet" />
+        <link rel="icon" href="/images/mediapluslogo.png" />
+      </Head>
+      <body>
         <Header />
-        <body>
         {children}
         <Footer />
-        </body>
-        </>
-    )
+      </body>
+    </>
+  )
 }
 
 export default Layout

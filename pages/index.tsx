6ce18cd5cd61154eec3a5c5cf005e557b1dfdf42
Muhammad <jamiludeensani@gmail.com>
2023-01-04 18:21:57 +0100

    new update

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,8 @@ import Layout from "@components/layout";
 import ContactForm from "@components/contact-form";
 import Section from "@components/section";
 import Portfolio from "@components/section/portfolio";
-import { http } from "../utils/utils";
 
 const Home: React.FC = (): JSX.Element => {
-  useEffect(() => {
-    http.get('/');
-  }, []);
   return (
     <div className="d-flex flex-column min-vh-100">
       <Layout>
